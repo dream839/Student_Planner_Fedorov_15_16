1f699284d848b0bcfa36d11bab36f49c276eeb10
dream839 <mihailfedorov3973@gmail.com>
2026-05-16 12:16:26 +0300

    Step 7: Setup NavHost and complete navigation

--- app/src/main/java/com/fedorov/student_planner/MainActivity.kt
+++ app/src/main/java/com/fedorov/student_planner/MainActivity.kt
@@ -12,19 +12,20 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.fedorov.student_planner.ui.theme.Student_PlannerTheme
+import androidx.navigation.compose.rememberNavController
+import com.fedorov.student_planner.navigation.StudentPlannerNavHost
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-            Student_PlannerTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
+            val navController = rememberNavController()
+            Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                StudentPlannerNavHost(
+                    navController = navController,
+                    modifier = Modifier.padding(innerPadding)
+                )
             }
         }
     }
